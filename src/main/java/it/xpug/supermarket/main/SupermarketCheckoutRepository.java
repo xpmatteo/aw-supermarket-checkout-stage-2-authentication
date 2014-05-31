1741703b1ce5777956190d34167afac0479d0bc0
Matteo Vaccari <vaccari@pobox.com>
2014-05-31 16:46:58 +0200

    create the whole aggregate inside the repository

--- src/main/java/it/xpug/supermarket/main/SupermarketCheckoutRepository.java
+++ src/main/java/it/xpug/supermarket/main/SupermarketCheckoutRepository.java
@@ -4,15 +4,14 @@ import it.xpug.generic.db.*;
 
 public class SupermarketCheckoutRepository {
 
-	private PriceList priceList;
 	private Database database;
 
-	public SupermarketCheckoutRepository(PriceList priceList, Database database) {
-		this.priceList = priceList;
+	public SupermarketCheckoutRepository(Database database) {
 		this.database = database;
 	}
 
 	public SupermarketCheckout findById(int supermarketCheckoutId) {
+		PriceList priceList = new DatabasePriceList(database);
 		SupermarketCheckout checkout = new SupermarketCheckout(priceList);
 		// do something with the database ...
 		checkout.setTotal(1234);
--- src/main/java/it/xpug/supermarket/main/SupermarketServlet.java
+++ src/main/java/it/xpug/supermarket/main/SupermarketServlet.java
@@ -10,18 +10,16 @@ import javax.servlet.http.*;
 
 public class SupermarketServlet extends HttpServlet {
 
-	private PriceList priceList;
 	private Database database;
 
 	public SupermarketServlet() {
 		DatabaseConfiguration configuration = new DatabaseConfiguration("database.properties");
 		database = new Database(configuration);
-		priceList = new DatabasePriceList(database);
 	}
 
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		SupermarketCheckoutRepository repository = new SupermarketCheckoutRepository(priceList, database);
+		SupermarketCheckoutRepository repository = new SupermarketCheckoutRepository(database);
 		SupermarketCheckout checkout = repository.findById(0);
 		SupermarketController controller = new SupermarketController(checkout , request, response);
 		controller.service();
